00dc29981c80243791af89dc061be8380efdb211
Thomas M. Rehnert <tomrehnert2011@gmail.com>
2016-04-07 12:22:35 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+	root to: 'tags#home'
+	get '/' => 'tags#home'
+	get '/tags' => 'tags#index'
+	get '/tags/:id' => 'tags#show', as: :tag
+	get 'destinations/:id' => 'destinations#show', as: :destination
+
+	get '/destinations/:id/edit' => 'destinations#edit', as: :edit_destination 
+	patch '/destinations/:id' => 'destinations#update'
+end
